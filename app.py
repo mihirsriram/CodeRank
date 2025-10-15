1aac980dcc8b578ac6615e392f8612c17fba7234
Mihir Sriram <53307630+mihirsriram@users.noreply.github.com>
2025-10-15 19:22:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # Launch Streamlit from within the package
 subprocess.run([
     "streamlit", "run", "coderank_lc/ui/streamlit_app.py",
-    "--server.port=7860", "--server.address=0.0.0.0"
+    "--server.port=8502", "--server.address=0.0.0.0"
 ])
